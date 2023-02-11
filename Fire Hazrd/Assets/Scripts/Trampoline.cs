938d6a7f1d31eef992625a56eb8d3ac88ff7651d
ORTIGAgames <marcos29diciembre@gmail.com>
2023-02-11 16:44:19 +0100

    Trampoline

--- Fire Hazrd/Assets/Scripts/Trampoline.cs
+++ Fire Hazrd/Assets/Scripts/Trampoline.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ManagerGame : MonoBehaviour
+public class Trampoline : MonoBehaviour
 {
     public float launchForce;
 
