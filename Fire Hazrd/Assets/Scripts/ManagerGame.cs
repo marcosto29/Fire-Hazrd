115026915989a3523564164808975bf120e11d8c
ORTIGAgames <marcos29diciembre@gmail.com>
2023-02-12 18:07:13 +0100

    uebo

--- Fire Hazrd/Assets/Scripts/ManagerGame.cs
+++ Fire Hazrd/Assets/Scripts/ManagerGame.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class ManagerGame : MonoBehaviour
 {
+    string ActiveLayer;
+    int ActiveLayerId;
+    public Player MainCharacter;
+    public GameObject[] Layers;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ActiveLayer = MainCharacter.gameObject.GetComponent<SpriteRenderer>().sortingLayerName;
+        ActiveLayerId = MainCharacter.gameObject.GetComponent<SpriteRenderer>().sortingLayerID;
+        ActiveLayers();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ActiveLayers()
+    {
+        foreach(GameObject l in Layers)
+        {
+            if(l.gameObject.GetComponent<SpriteRenderer>().sortingLayerName == ActiveLayer)
+            {
+                foreach(Transform child in l.transform)
+                {
+                    if (child.GetComponent<Collider2D>())
+                    {
+                        Collider2D[] colList = child.transform.GetComponentsInChildren<Collider2D>();
+                        foreach (Collider2D c in colList)
+                        {
+                            c.enabled = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (Transform child in l.transform)
+                {
+                    if (child.GetComponent<Collider2D>())
+                    {
+                        Collider2D[] colList = child.transform.GetComponentsInChildren<Collider2D>();
+                        foreach(Collider2D c in colList)
+                        {
+                            c.enabled = false;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- Fire Hazrd/Assets/Scripts/Player.cs
+++ Fire Hazrd/Assets/Scripts/Player.cs
@@ -19,7 +19,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Damage()
