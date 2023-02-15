fe271502aebbb57b1ded9f7ead973741358e32a8
Yibus13 <jesusculebras.scf@gmail.com>
2023-02-15 19:32:41 +0100

    ya

--- Fire Hazrd/Assets/Scripts/GameOver.cs
+++ Fire Hazrd/Assets/Scripts/GameOver.cs
@@ -7,11 +7,10 @@ public class GameOver : MonoBehaviour
     // Start is called before the first frame update
     public void Restart()
     {
-        GlobalInventory.flags[0] = false;
-        GlobalInventory.flags[1] = false;
-        GlobalInventory.flags[2] = false;
-        GlobalInventory.flags[3] = false;
-        GlobalInventory.end = false;
+        GlobalInventory.animationflags[0] = false;
+        GlobalInventory.animationflags[1] = false;
+        GlobalInventory.animationflags[2] = false;
+        GlobalInventory.animationflags[3] = false;
         GlobalInventory.start = true;
     }
 
